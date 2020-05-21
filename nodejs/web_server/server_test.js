12b4a11ffd913658e6820aee13edc719e53ace94
kanic1111 <daniel890723@gmail.com>
2020-05-21 17:20:42 +0800

    web socket test

--- nodejs/web_server/server_test.js
+++ nodejs/web_server/server_test.js
@@ -17,7 +17,7 @@ arduinoport.on('open',function() {
         parser.on('data', line =>{
             console.log(line)  
     setTimeout(function() {
-        ws.send(line)
+        ws.send(line)//傳給client端serialport讀的到訊息
     },1000)
 })
     ws.on('message' , message =>{
@@ -25,10 +25,9 @@ arduinoport.on('open',function() {
         var data1 = JSON.parse(message); //將client端傳來的訊息包成JSON給變數data1
         console.log(data1);//在終端顯示出data1
         if ( data1.number == '1410732051') {
-             clients.forEach(client => {
-            client.send(String('hello 魯志謙'))
+             clients.forEach(client => { 
+            client.send(String('hello 魯志謙'))  
              })
-            //ws.send(String('hello 魯志謙'))  將data1裡的number欄位的資料拿出來 如果一樣做括號裡的內容
         }
         else if ( data1.number == '1410732001') {
             clients.forEach(client => {
